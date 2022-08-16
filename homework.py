03eeb00828bd3c908357609a77043c78b3ecab25
Denis Kokorin <dk300den@yandex.ru>
2022-08-16 21:18:19 +0300

    Telegram-Bot (исправления после ревью)

--- homework.py
+++ homework.py
@@ -36,37 +36,46 @@ HOMEWORK_STATUSES = {
 
 class AnswerStatusIsNot200Error(Exception):
     """Статус ответа не равен значению 200."""
+
     def __init__(self, *args):
+        """Формирует сообщение об ошибке."""
         if args:
             self.message = args[0]
         else:
             self.message = self.__doc__
 
     def __str__(self):
+        """Выводит сообщение об ошибке."""
         return f'{self.message} '
 
 
 class RequestReceivingError(Exception):
     """Ошибка получения ответа на запрос."""
+
     def __init__(self, *args):
+        """Формирует сообщение об ошибке."""
         if args:
             self.message = args[0]
         else:
             self.message = self.__doc__
 
     def __str__(self):
+        """Выводит сообщение об ошибке."""
         return f'{self.message}'
 
 
 class IncorrectDataTypeError(Exception):
     """Некорректные тип данных в ответе."""
+
     def __init__(self, *args):
+        """Формирует сообщение об ошибке."""
         if args:
             self.message = args[0]
         else:
             self.message = self.__doc__
 
     def __str__(self):
+        """Выводит сообщение об ошибке."""
         return f'{self.message}'
 
 
