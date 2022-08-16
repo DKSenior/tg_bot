fec81d2e52de1d25ce05776cc54efb36ef2adcb8
Denis Kokorin <dk300den@yandex.ru>
2022-08-17 00:40:38 +0300

    Telegram-Bot (исправления после ревью)

--- homework.py
+++ homework.py
@@ -116,7 +116,6 @@ def get_api_answer(current_timestamp):
 def check_response(response):
     """Проверяет ответ API на корректность."""
     logger.info('Проверка корректности ответа')
-
     if not isinstance(response['homeworks'][0], dict):
         raise IncorrectDataTypeError(
             f'У ответа некорректный тип данных: {type(response)}'
